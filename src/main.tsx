3b67f3d6bc74f754a362cb360b4871e90d19722b
Guilherme <guilherme@alume.com>
2024-03-27 22:48:12 -0300

    install and config react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+import { ThemeProvider } from 'styled-components'
+import { AppRouter } from './router.tsx'
+import { theme, GlobalStyle } from './styles'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+
+      <AppRouter />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { PostsPage } from './pages/Posts'
+import { PostPage } from './pages/Post'
+
+export function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/posts" />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/posts/:id" element={<PostPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
