20d5b05a24cf649be19540e0e5481d572e4ec584
Guilherme <guilherme@alume.com>
2024-03-27 22:48:55 -0300

    create post

---
+++ src/pages/Post/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from '../../components/Container'
+import { Header } from '../../components/Header'
+
+export function PostPage() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Container></Container>
+      </main>
+    </>
+  )
+}
