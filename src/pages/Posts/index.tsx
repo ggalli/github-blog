8f14c0c0c68e2691d41a06381bb556238880b4bc
Guilherme <guilherme@alume.com>
2024-04-01 21:52:12 -0300

    move posts component content to posts page

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,7 +1,9 @@
 import { Container } from '../../components/Container'
 import { Header } from '../../components/Header'
-import { Posts } from '../../components/Posts'
+import { Input } from '../../components/Input'
+import { PostCard } from '../../components/PostCard'
 import { Profile } from '../../components/Profile'
+import { PostsContainer, PostsSearchField } from './styles'
 
 export function PostsPage() {
   return (
@@ -11,7 +13,23 @@ export function PostsPage() {
       <main>
         <Container>
           <Profile />
-          <Posts />
+
+          <PostsSearchField>
+            <h2>
+              Publicações <span>6 publicações</span>
+            </h2>
+
+            <Input placeholder="Buscar conteúdo" />
+          </PostsSearchField>
+
+          <PostsContainer>
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+          </PostsContainer>
         </Container>
       </main>
     </>
