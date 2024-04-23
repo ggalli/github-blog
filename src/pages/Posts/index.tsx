f8382e647acb32916b7f882744ddde813ffef089
Guilherme <guilherme@alume.com>
2024-04-22 23:25:44 -0300

    get and display posts

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -2,29 +2,51 @@ import { Input } from '../../components/Input'
 import { Layout } from '../../components/Layout'
 import { PostCard } from '../../components/PostCard'
 import { Profile } from '../../components/Profile'
+import { useFetch } from '../../hooks/use-fetch'
 import { PostsContainer, PostsSearchField } from './styles'
 
+interface Issue {
+  created_at: string
+  title: string
+  number: string
+  body: string
+  id: string
+}
+
 export function PostsPage() {
+  const { data, error, isLoading } = useFetch<Issue[]>('/issues')
+
   return (
     <Layout>
       <Profile />
 
-      <PostsSearchField>
-        <h2>
-          Publicações <span>6 publicações</span>
-        </h2>
+      {error ? (
+        'Falha ao carregar publicações'
+      ) : isLoading ? (
+        'Loading...'
+      ) : (
+        <>
+          <PostsSearchField>
+            <h2>
+              Publicações <span>{data?.length} publicações</span>
+            </h2>
 
-        <Input placeholder="Buscar conteúdo" />
-      </PostsSearchField>
+            <Input placeholder="Buscar conteúdo" />
+          </PostsSearchField>
 
-      <PostsContainer>
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-      </PostsContainer>
+          <PostsContainer>
+            {data?.map((item) => (
+              <PostCard
+                key={item.id}
+                title={item.title}
+                text={item.body}
+                number={item.number}
+                created_at={item.created_at}
+              />
+            ))}
+          </PostsContainer>
+        </>
+      )}
     </Layout>
   )
 }
