e55ba3f7f4680d49a931fce6ba02b471c8eb71ea
Guilherme <guilherme@alume.com>
2024-04-22 23:24:56 -0300

    implement props on post card component

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,23 +1,24 @@
 import { PostCardContainer } from './styles'
 import { Text } from '../Text'
 
-export function PostCard() {
+interface PostCardProps {
+  created_at: string
+  title: string
+  number: string
+  text: string
+}
+
+export function PostCard({ title, text, number }: PostCardProps) {
   return (
-    <PostCardContainer>
+    <PostCardContainer to={`/posts/${number}`}>
       <h3>
-        JavaScript data types and data structures
+        {title}
         <Text size="sm" as="span">
           Há 1 dia
         </Text>
       </h3>
 
-      <Text>
-        Programming languages all have built-in data structures, but these often
-        differ from one language to another. This article attempts to list the
-        built-in data structures available in JavaScript and what properties
-        they have. These can be used to build other data structures. Wherever
-        possible, comparisons with other languages are drawn.
-      </Text>
+      <Text>{text}</Text>
     </PostCardContainer>
   )
 }
--- src/components/PostCard/styles.ts
+++ src/components/PostCard/styles.ts
@@ -1,13 +1,15 @@
 import styled from 'styled-components'
 import { Text } from '../Text'
+import { Link } from 'react-router-dom'
 
-export const PostCardContainer = styled.div`
+export const PostCardContainer = styled(Link)`
   width: 100%;
   height: 260px;
   padding: 32px;
   border-radius: 10px;
   background-color: ${({ theme }) => theme['primary-600']};
   cursor: pointer;
+  text-decoration: none;
 
   &:hover {
     box-shadow: 0 0 0 2px ${({ theme }) => theme['primary-400']};
@@ -33,5 +35,6 @@ export const PostCardContainer = styled.div`
     overflow: hidden;
     text-overflow: ellipsis;
     display: -webkit-box;
+    color: ${({ theme }) => theme['primary-200']};
   }
 `
