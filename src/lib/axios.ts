f38f307cf7a0d456415cf70bb41bf0b29e1efd15
Guilherme <guilherme@alume.com>
2024-04-23 22:10:40 -0300

    github url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://66270a86b625bf088c0730b7.mockapi.io/api',
-  // baseURL: 'https://api.github.com/',
+  // baseURL: 'https://github-blog.free.beeceptor.com',
+  baseURL: 'https://api.github.com/',
 })
