8304857bd7f16e6e24c2913892a6bdea6c2ee604
Guilherme <guilherme@alume.com>
2024-04-23 22:13:30 -0300

    fix route

--- src/router.tsx
+++ src/router.tsx
@@ -8,7 +8,7 @@ export function AppRouter() {
       <Routes>
         <Route path="/" element={<Navigate replace to="/posts" />} />
         <Route path="/posts" element={<PostsPage />} />
-        <Route path="/posts/:id" element={<PostPage />} />
+        <Route path="/posts/:postId" element={<PostPage />} />
       </Routes>
     </BrowserRouter>
   )
