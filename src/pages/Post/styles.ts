13d80fef7f7666f07b01626dd087425c297c0678
Guilherme <guilherme@alume.com>
2024-04-28 21:55:55 -0300

    fix github api endpoint and markdown styles

--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -3,4 +3,23 @@ import styled from 'styled-components'
 export const PostContent = styled.div`
   padding: 0 40px;
   margin-top: -48px;
+
+  .markdown {
+    img {
+      width: 100%;
+    }
+
+    p,
+    h1,
+    h2,
+    h3,
+    ul {
+      margin-bottom: 16px;
+    }
+
+    ul {
+      list-style: inside;
+      margin-left: 16px;
+    }
+  }
 `
